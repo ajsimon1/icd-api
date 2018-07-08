043934e3c5a7e5fa993149a17ca228069b1674fa
Adam <adamjsimon1@yahoo.com>
2018-07-08 19:42:11 -0400

    added table to hold results

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
+$(document).ready(function () {
+  $(".table").hide();
+});
+
 var req = require('request');
-
 var base = 'https://clinicaltables.nlm.nih.gov/api/icd10cm/v3/search';
-var quer = base + '?sf=code,name&terms=tuberc';
-var a;
-
-var options = {
-  uri: quer,
-  method: 'GET',
-}
 
 // can pass params in as qs option
 
 function pingApi() {
-  req(quer, function (err, resp, body) {
-    let a = JSON.parse(body);
-    document.getElementById('test').innerHTML = a;
-})};
+  var a;
+  var icd = $("#icd-name").val();
+
+  var options = {
+    url: base,
+    method: 'GET',
+    qs: {
+      sf: 'code,name',
+      terms: icd
+    }
+  }
+
+  req(options, function (err, resp, body) {
+    a = JSON.parse(body)[3];
+
+    var table = document.getElementById('results-table');
+    for (var i = 1; i < table.rows.length; i++) {
+      for (var j = 0; table.rows[0].cells.length; j++) {
+        table.rows[i].cells[j].innerHTML = a[i-1][j];
+      }}
+  })
+  $("#results-table").show();
+};
 
 function clearInput() {
   document.getElementById('test').innerHTML = "";
+  $("#results-table").hide();
 };
