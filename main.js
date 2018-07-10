515c905f3af997fb034342d7cb6f211267538c70
beachboy732 <adamjsimon1@yahoo.com>
2018-07-10 12:52:09 -0400

    added shitty icon

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow() {
   win = new BrowserWindow({
     width: 800,
     height: 500,
-    // icon: path.join(__dirname, 'icon.jpg'),
+    icon: path.join(__dirname, 'icd_image.jpg'),
   })
   win.loadURL(url.format ({
     pathname: path.join(__dirname, 'index.html'),
